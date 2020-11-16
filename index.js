d6f3993b280662736f5060a02adb32c380d4e421
carina092 <carina@carinascheiberlich.nl>
2020-11-16 19:01:51 +0100

    Pen pineapple apple pen.

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const score = document.querySelector('#score');
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
-let width = 10;
+const width = 10;
+let appleIndex = 0;
 
 function createGrid() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < width*width; i++) {
         const square = document.createElement('div');
 
         square.classList.add('square');
@@ -23,16 +24,43 @@ createGrid();
 currentSnake.forEach(index => squares[index].classList.add('snake'));
 
 function move() {
+    if (
+        (currentSnake[0] + width >= width*width && direction === width) ||
+        (currentSnake[0] % width === width-1 && direction === 1) ||
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (currentSnake[0] - width < 0 && direction === -width) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId);
+    
     const tail = currentSnake.pop();
     squares[tail].classList.remove('snake');
 
     currentSnake.unshift(currentSnake[0] + direction);
+
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        squares[currentSnake[0]].classList.remove('apple');
+        squares[tail].classList.add('snake');
+        currentSnake.push(tail);
+        generateApples();
+    }
+
+
     squares[currentSnake[0]].classList.add('snake');
 }
 
 move();
 
 let timerId = setInterval(move, 1000);
+
+function generateApples() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length);
+    } while (squares[appleIndex].classList.contains('snake'));
+    squares[appleIndex].classList.add('apple');
+}
+
+generateApples();
 
 function control(e) {
     if (e.keyCode === 39) {
